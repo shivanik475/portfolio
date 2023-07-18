b8bcc6664231e08bbe62309629bf0ca12e685ae2
shivanik475 <54467955+shivanik475@users.noreply.github.com>
2023-07-19 01:33:38 +0530

    fixes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,35 +1,48 @@
 import React from "react";
 import logo from "../asset/logo.jpeg";
-import { MdKeyboardArrowRight } from "react-icons/md";
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+import { Link } from "react-scroll";
 
-export default function Home() {
+const Home = () => {
   return (
     <div
       name="home"
-      className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800 text-white"
+      className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800"
     >
-      <div className="max-w-screen-lg mx-auto flex-col items-center justify-center h-full px-4 md:flex-row">
+      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full">
           <h2 className="text-4xl sm:text-7xl font-bold text-white">
-            i'm a frontend developer
+            I'm a Frontend Developer
           </h2>
           <p className="text-gray-500 py-4 max-w-md">
-            I have 6 years of experience building and desiging software
+            I have 6 years of experience building and desgining software.
           </p>
+
           <div>
-            <button className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyn-500 to-blue-500 cursor-pointer">
-              Portfolio<span className="group-hover:rotate-90 duration-300">{MdKeyboardArrowRight}</span>
-            </button>
+            <Link
+              to="portfolio"
+              smooth
+              duration={500}
+              className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
+            >
+              Portfolio
+              <span className="group-hover:rotate-90 duration-300">
+                <MdOutlineKeyboardArrowRight size={25} className="ml-1" />
+              </span>
+            </Link>
           </div>
         </div>
+
         <div>
-          {/*<img
+          <img
             src={logo}
             alt="my profile"
-            className="rounded-1xl mx-auto w-2/3 md:w-small smaller-size"
-          ></img>*/}
+            className="rounded-2xl mx-auto w-2/3 md:w-full"
+          />
         </div>
       </div>
     </div>
   );
-}
+};
+
+export default Home;
