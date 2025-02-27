957819c2c0bfb8819396c375c037769b932e9009
Shivani Kumari <54467955+shivanik475@users.noreply.github.com>
2025-02-27 18:44:12 +0000

    css update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,18 +1,25 @@
-import React from 'react'
+import React from "react";
 
 export default function About() {
-    return (
-        <div name="about" className="w-full h-screen bg-gradient-to-b from-gray-500 to-black text-white">
-        <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
-            <div className="pb-8">
-                <p className="text-4xl font-bold inline border-b-4 border-gray-500">About</p>
-            </div>
-            <p className="text-xl mt-20">
-             A passionate Frontend Developer with experience in creating dynamic, responsive web applications using React and modern web technologies.Always eager to learn and improve, I’m dedicated to refining my craft and tackling new challenges in web development.
-            </p>
-            <br/>
+  return (
+    <div
+      name="about"
+      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            About
+          </p>
         </div>
-            
-        </div>
-    )
+        <p className="text-xl mt-20">
+          A passionate Frontend Developer with experience in creating dynamic,
+          responsive web applications using React and modern web
+          technologies.Always eager to learn and improve, I’m dedicated to
+          refining my craft and tackling new challenges in web development.
+        </p>
+        <br />
+      </div>
+    </div>
+  );
 }
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -9,66 +9,38 @@ import html from "../asset/HTML.png";
 
 export default function Experience() {
   const techs = [
-    {
-      id: 1,
-      src: react,
-      title: "React JS",
-    },
-    {
-      id: 2,
-      src: css,
-      title: "CSS",
-    },
-    {
-      id: 3,
-      src: node,
-      title: "Node JS",
-    },
-    {
-      id: 4,
-      src: tailwind,
-      title: "Tailwind",
-    },
-    {
-      id: 5,
-      src: github,
-      title: "GitHub",
-    },
-    {
-      id: 6,
-      src: javascript,
-      title: "JavaScript",
-    },
-    {
-      id: 7,
-      src: html,
-      title: "HTML",
-    },
+    { id: 1, src: react, title: "React JS" },
+    { id: 2, src: css, title: "CSS" },
+    { id: 3, src: node, title: "Node JS" },
+    { id: 4, src: tailwind, title: "Tailwind" },
+    { id: 5, src: github, title: "GitHub" },
+    { id: 6, src: javascript, title: "JavaScript" },
+    { id: 7, src: html, title: "HTML" },
   ];
+
   return (
     <div
       name="experience"
-      className="bg-gradient-to-b from-gray-800 to-black w-full h-screen"
+      className="bg-gradient-to-b from-gray-800 to-black w-full text-white md:h-screen"
     >
-      <div className="max-w-screen-lg mx-uto p-4 flex flex-col justify-center w-full h-full text-white">
+      <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div>
           <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline">
             Skills
           </p>
-          <p className="py-6">These are the technologies i have worked with</p>
+          <p className="py-6">These are the technologies I have worked with</p>
         </div>
-        <div className="w-full flex justify-center items-center">
-          <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-8 text-center py-8 px-12 sm:px-0 place-items-center">
-            {techs.map(({ id, src, title }) => (
-              <div
-                key={id}
-                className="shadow-md hover:scale-105 duration-500 py-2 rounded-lg place-items-center"
-              >
-                <img src={src} alt="" className="w-20 mx-auto"></img>
-                <p className="mt-4">{title}</p>
-              </div>
-            ))}
-          </div>
+        {/* Grid container */}
+        <div className="grid grid-cols-2 sm:grid-cols-4 gap-8 text-center py-8 px-12 sm:px-0 place-items-center">
+          {techs.map(({ id, src, title }) => (
+            <div
+              key={id}
+              className="shadow-md hover:scale-105 duration-500 py-2 rounded-lg"
+            >
+              <img src={src} alt={title} className="w-20 mx-auto" />
+              <p className="mt-4">{title}</p>
+            </div>
+          ))}
         </div>
       </div>
     </div>
