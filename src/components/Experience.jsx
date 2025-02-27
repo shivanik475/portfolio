cfe9abb4b9ccd46b0e8748e0132108227d5b2b50
Shivani Kumari <54467955+shivanik475@users.noreply.github.com>
2025-02-27 18:30:04 +0000

    changes

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -55,20 +55,20 @@ export default function Experience() {
           <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline">
             Skills
           </p>
-          <p className="py-6">
-            These are the technologies i have worked with
-          </p>
+          <p className="py-6">These are the technologies i have worked with</p>
         </div>
-        <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0 place-items-center">
-          {techs.map(({ id, src, title }) => (
-            <div
-              key={id}
-              className="shadow-md hover:scale-105 duration-500 py-2 rounded-lg"
-            >
-              <img src={src} alt="" className="w-20 mx-auto"></img>
-              <p className="mt-4">{title}</p>
-            </div>
-          ))}
+        <div className="w-full flex justify-center items-center">
+          <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-8 text-center py-8 px-12 sm:px-0 place-items-center">
+            {techs.map(({ id, src, title }) => (
+              <div
+                key={id}
+                className="shadow-md hover:scale-105 duration-500 py-2 rounded-lg place-items-center"
+              >
+                <img src={src} alt="" className="w-20 mx-auto"></img>
+                <p className="mt-4">{title}</p>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
