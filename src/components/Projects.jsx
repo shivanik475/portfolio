c8f1e8ee889ab41374605d4a071fc8dfdcd47da1
Shivani Kumari <54467955+shivanik475@users.noreply.github.com>
2025-04-21 06:53:30 +0000

    new project added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -50,6 +50,11 @@ export default function Projects() {
       src: "https://raw.githubusercontent.com/kyle-barth/tic-tac-toe/master/tic-tac-toe.PNG",
       link: "https://github.com/shivanik475/TicTacToe",
     },
+    {
+      id: 9,
+      src: "https://elegant-engelbart-b9c316.netlify.app/static/media/logo.b5048097.png",
+      link: "https://github.com/shivanik475/Dictionary",
+    },
   ];
   return (
     <div
@@ -63,20 +68,23 @@ export default function Projects() {
           </p>
           <p className="py-6">Check out some of my work right here</p>
         </div>
-        <div className="grid sm:grid-cols-3 md-grid-cols-3 gap-8 px-6 sm:px-0 ">
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-6 sm:px-0">
           {portfolios.map(({ id, src, link }) => (
-            <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
+            <div
+              key={id}
+              className="shadow-md shadow-gray-600 rounded-lg overflow-hidden hover:shadow-xl transition duration-300"
+            >
               <img
                 src={src}
                 alt=""
-                className="rounded-md duration-200 hover:scale-105"
-              ></img>
-              <div className="text-center mt-3">
+                className="w-full h-48 object-cover hover:scale-105 transition-transform duration-300"
+              />
+              <div className="text-center mt-3 mb-4">
                 <a
                   href={link}
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="text-white-500 hover:underline font-semibold"
+                  className="text-blue-400 hover:underline font-semibold"
                 >
                   View Project
                 </a>
